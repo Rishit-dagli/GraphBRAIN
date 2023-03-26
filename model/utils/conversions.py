28abde0c149f9d261ae768b584712aae513370ed
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-26 18:12:58 +0000

    Fix minor issues with representations

--- model/utils/conversions.py
+++ model/utils/conversions.py
@@ -2,6 +2,7 @@ import rdkit
 from rdkit import Chem
 from .apply_feature_encoder import atom_feature_encoder, bond_feature_encoder
 import tensorflow as tf
+import einops
 
 
 def _smile_to_molecule(smile: str):
@@ -37,15 +38,24 @@ def smile_to_graph(smiles):
     atoms = []
     bonds = []
     pairs = []
+    # flag = False
 
     for smiles in smiles:
         molecule = _smile_to_molecule(smiles)
         atom_features, bond_features, pair_indices = _molecule_to_graph(molecule)
 
+        # if tf.rank(atom_features) == 2 and tf.rank(bond_features) == 2:
+        #         atom_features = einops.rearrange(atom_features, 'n d -> d n')
+        #         bond_features = einops.rearrange(bond_features, 'n d -> d n')
+        #         flag = True
+
         atoms.append(atom_features.numpy())
         bonds.append(bond_features.numpy())
         pairs.append(pair_indices.numpy())
 
+    # atoms = einops.rearrange(atoms, 'n d l -> n l d')
+    # bonds = einops.rearrange(bonds, 'n d l -> n l d')
+    # pairs = einops.rearrange(pairs, 'n l d -> n l d')
     # Ragged tensors are used to represent the variable length of the molecules.
     # This is a requirement for the graph neural network, since the only reason
     # we use GNNs in the foirst place is to handle variable length input.
