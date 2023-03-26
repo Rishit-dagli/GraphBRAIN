5d5a7d2d7d65f8e4ab28ed08deb872a2c2ed4a46
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-26 17:03:28 +0000

    Add support for molecule to graphs

--- model/utils/conversions.py
+++ model/utils/conversions.py
@@ -1,5 +1,25 @@
 import rdkit
 from rdkit import Chem
+from .apply_feature_encoder import atom_feature_encoder, bond_feature_encoder
+import tensorflow as tf
 
 def smile_to_molecule(smile: str):
     return Chem.MolFromSmiles(smile, sanitize=True)
+
+def molecule_to_graph(molecule: Chem.rdchem.Mol):
+    atoms = []
+    bonds = []
+    pairs = []
+    atom_encoder = atom_feature_encoder()
+    bond_encoder = bond_feature_encoder()
+
+    for atom in molecule.GetAtoms():
+        atoms.append(atom_encoder.encode(atom))
+        pairs.append([atom.GetIdx(), atom.GetIdx()])
+        bonds.append(bond_encoder.encode(None))
+        for neighbor in atom.GetNeighbors():
+            bond = molecule.GetBondBetweenAtoms(atom.GetIdx(), neighbor.GetIdx())
+            pairs.append([atom.GetIdx(), neighbor.GetIdx()])
+            bonds.append(bond_encoder.encode(bond))
+
+    return tf.constant(atoms), tf.constant(bonds), tf.constant(pairs)
