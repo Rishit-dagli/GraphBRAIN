7adc17716c794ce427028182cd0e7505445e2696
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-26 20:20:03 +0000

    Add and EdgeNetwork as a TensorFlow layer

--- model/training/build_model.py
+++ model/training/build_model.py
@@ -1,11 +1,44 @@
 import tensorflow as tf
+import einops
 
+class EdgeNetwork(tf.keras.layers.Layer):
+    def build(self, input_shape, kernel_initializer="glorot_uniform", bias_initializer="zeros"):
+        self.atom_dim = input_shape[0][-1]
+        self.bond_dim = input_shape[1][-1]
+        self.kernel = self.add_weight(
+            shape=(self.bond_dim, self.atom_dim * self.atom_dim),
+            initializer=kernel_initializer,
+            name="kernel",
+        )
+        self.bias = self.add_weight(
+            shape=(self.atom_dim * self.atom_dim), initializer=bias_initializer, name="bias",
+        )
+        self.built = True
+
+    def call(self, inputs):
+        atom_features, bond_features, pair_indices = inputs
+        bond_features = tf.einsum('ik,kj->ij', [bond_features, self.kernel]) + self.bias
+        bond_features = tf.reshape(bond_features, (-1, self.atom_dim, self.atom_dim))
+        atom_features_neighbors = tf.gather(atom_features, pair_indices[:, 1])
+        atom_features_neighbors = einops.rearrange(atom_features_neighbors, '... -> ... ()')
+        transformed_features = tf.einsum('ik,kj->ij', [bond_features, atom_features_neighbors])
+        transformed_features = einops.rearrange(transformed_features,  "... () -> ...")
+        aggregated_features = tf.math.unsorted_segment_sum(
+            transformed_features,
+            pair_indices[:, 0],
+            num_segments=tf.shape(atom_features)[0],
+        )
+        return aggregated_features
+
+
+
+    
 class TransformerEncoderReadout(tf.keras.layers.Layer):
     def __init__(
         self, num_heads=8, embed_dim=64, dense_dim=512, batch_size=32, **kwargs
     ):
         super().__init__(**kwargs)
-        
+
         self.partition_padding = PartitionPadding(batch_size)
         self.attention = tf.keras.layers.MultiHeadAttention(num_heads, embed_dim)
         self.dense_proj = tf.keras.Sequential(
@@ -46,3 +79,26 @@ class PartitionPadding(tf.keras.layers.Layer):
         gather_indices = tf.where(tf.reduce_sum(atom_features_stacked, (1, 2)) != 0)
         gather_indices = tf.squeeze(gather_indices, axis=-1)
         return tf.gather(atom_features_stacked, gather_indices, axis=0)
+
+def create_model(
+    atom_dim,
+    bond_dim,
+    batch_size=32,
+    message_units=64,
+    message_steps=4,
+    num_attention_heads=8,
+    dense_units=[512],
+    activation=["relu"],
+):
+    atom_features = tf.keras.layers.Input((atom_dim), dtype="float32", name="atom_features")
+    bond_features = tf.keras.layers.Input((bond_dim), dtype="float32", name="bond_features")
+    pair_indices = tf.keras.layers.Input((2), dtype="int32", name="pair_indices")
+    molecule_indicator = tf.keras.layers.Input((), dtype="int32", name="molecule_indicator")
+
+    x = MessagePassing(message_units, message_steps)(
+        [atom_features, bond_features, pair_indices]
+    )
+
+    x = TransformerEncoderReadout(
+        num_attention_heads, message_units, dense_units, batch_size
+    )([x, molecule_indicator])
