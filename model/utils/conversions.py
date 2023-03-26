936feda0dc5a11141dcb2b209a184e5d2546b889
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-26 17:23:54 +0000

    Put together previous function to get graphs from list of smiles

--- model/utils/conversions.py
+++ model/utils/conversions.py
@@ -4,11 +4,11 @@ from .apply_feature_encoder import atom_feature_encoder, bond_feature_encoder
 import tensorflow as tf
 
 
-def smile_to_molecule(smile: str):
+def _smile_to_molecule(smile: str):
     return Chem.MolFromSmiles(smile, sanitize=True)
 
 
-def molecule_to_graph(molecule: Chem.rdchem.Mol):
+def _molecule_to_graph(molecule: Chem.rdchem.Mol):
     atoms = []
     bonds = []
     pairs = []
@@ -24,5 +24,33 @@ def molecule_to_graph(molecule: Chem.rdchem.Mol):
             pairs.append([atom.GetIdx(), neighbor.GetIdx()])
             bonds.append(bond_encoder.encode(bond))
 
-    # This constitutes the major use of graaphs, each molecule is a graph with the atoms as nodes and the bonds as edges.
-    return tf.constant(atoms), tf.constant(bonds), tf.constant(pairs)
+    # This constitutes the major use of graaphs, each molecule is a graph with
+    # the atoms as nodes and the bonds as edges.
+    return (
+        tf.constant(atoms, dtype=tf.float32),
+        tf.constant(bonds, dtype=tf.float32),
+        tf.constant(pairs, dtype=tf.int64),
+    )
+
+
+def smile_to_graph(smiles):
+    atoms = []
+    bonds = []
+    pairs = []
+
+    for smiles in smiles:
+        molecule = _smile_to_molecule(smiles)
+        atom_features, bond_features, pair_indices = _molecule_to_graph(molecule)
+
+        atoms.append(atom_features)
+        bonds.append(bond_features)
+        pairs.append(pair_indices)
+
+    # Ragged tensors are used to represent the variable length of the molecules.
+    # This is a requirement for the graph neural network, since the only reason
+    # we use GNNs in the foirst place is to handle variable length input.
+    return (
+        tf.ragged.constant(atoms, dtype=tf.float32),
+        tf.ragged.constant(bonds, dtype=tf.float32),
+        tf.ragged.constant(pairs, dtype=tf.int64),
+    )
