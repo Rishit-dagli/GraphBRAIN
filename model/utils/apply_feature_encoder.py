9c34bebcd2179533271354803111c32df2bc68df
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-26 17:03:11 +0000

    Modify function naming

---
+++ model/utils/apply_feature_encoder.py
@@ -0,0 +1,11 @@
+from .feature_encoder import AtomFeatureEncoder, BondFeatureEncoder
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from configuration.training import atoms, bonds
+
+def atom_feature_encoder():
+    return AtomFeatureEncoder(allowed_feature_sets=atoms())
+
+def bond_feature_encoder():
+    return BondFeatureEncoder(allowed_feature_sets=bonds())
