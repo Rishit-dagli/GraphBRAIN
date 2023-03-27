9d4f1ad9f14a428baf8188eb132f9719668289cf
Rishit Dagli <rishit.dagli@gmail.com>
2023-03-27 04:10:05 +0000

    Format with black

--- model/inference/infer.py
+++ model/inference/infer.py
@@ -10,5 +10,7 @@ from dataset.loader import loader
 
 
 def predict(smile: list, model: tf.keras.Model) -> float:
-    smile_data = loader(smile_to_graph(smile), pd.Series([0.0]), batch_size=1, shuffle=False)
+    smile_data = loader(
+        smile_to_graph(smile), pd.Series([0.0]), batch_size=1, shuffle=False
+    )
     return tf.squeeze(model.predict(smile_data), axis=1)
