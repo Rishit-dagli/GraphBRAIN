0bf413749697336b6677ff4223cf7e1a30a50d13
Shivesh Prakash <shiveshprakash2@gmail.com>
2023-03-28 00:00:25 -0400

    Removing unnecessary imports

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import requests
 import streamlit as st
-import numpy as np
 from model.inference.load_model import load_model
 from model.inference.infer import predict
-from streamlit_lottie import st_lottie
 from PIL import Image
 
 
-logo = "http://store.rishit.tech/GraphBRAIN_logo.png"
 page_title = "Graph Brain"
 img = Image.open('media/Graph.png')
 
 
 # Set page title and favicon.
-st.set_page_config(page_title=page_title, page_icon=logo)
+st.set_page_config(page_title=page_title, page_icon=img)
 
 st.markdown(
     f"""
