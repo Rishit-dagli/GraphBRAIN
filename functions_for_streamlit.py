bdec6aaa5de6b0dbb76587ba645ae499dd7b8525
Shivesh Prakash <shiveshprakash2@gmail.com>
2023-03-29 00:35:56 -0400

    Adding pyta and documentation for functions_for_streamlit.py

--- functions_for_streamlit.py
+++ functions_for_streamlit.py
@@ -1,11 +1,58 @@
+"""CSC111 Winter 2023 Final Project: Graph Brain
+
+This Python file defines useful function to run the Streamlit app "Graph Brain".
+
+The application has the following functions:
+
+embed_molview(smile: str) -> None: This function takes a SMILES string representing a molecule and embeds a 3D rendering
+ of the molecule using the MolView API. If an error occurs, it displays an error message.
+
+load_model_in_cache() -> tf.keras.Model: This function loads a trained Keras model for predicting the permeability of
+molecules into the Streamlit cache. If the model is not in the cache, it loads it from a file using the load_model()
+function from the model.inference.load_model module.
+
+output_for_button(button_number: int, samples: list, model: tf.keras.Model) -> float: This function takes an integer
+representing the index of a sample SMILES string, a list of sample SMILES strings, and a Keras model, and returns the
+predicted permeability of the molecule represented by the sample SMILES string.
+
+output_for_string(smiles: str, model: tf.keras.Model) -> float: This function takes a SMILES string representing a
+molecule and a Keras model, and returns the predicted permeability of the molecule.
+
+display_desc_instr() -> None: This function displays the description and instructions for using the web application.
+The description explains what the blood-brain barrier is and why it is important. The instructions explain how to use
+the web application to predict the permeability of molecules and render them in 3D.
+
+display_goal() -> None: This function displays the goal of the project, which is to predict the permeability of
+molecules through the blood-brain barrier.
+
+set_background_black() -> None: This function sets the background of the web application to black.
+
+Copyright and Usage Information
+===============================
+This file is provided solely for the personal and private use of TAs, instructors and its author(s). All forms of
+distribution of this code, whether as given or with any changes, are expressly prohibited.
+
+This file is Copyright (c) 2023 by Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde."""
+
 import streamlit as st
 from model.inference.load_model import load_model
 from model.inference.infer import predict
 import tensorflow as tf
+import python_ta as pyta
 
 
 def embed_molview(smile: str) -> None:
-    """Embed a 3D rendering of the molecule with the given SMILES string."""
+    """This function embeds a 3D rendering of a molecule with the given SMILES string. It takes a single argument,
+    smile, which is a string representing the SMILES notation of the molecule to be rendered.
+    If successful, it will render a 3D visualization of the molecule using the MolView service, and embed it using the
+    st.components.v1.iframe() function. The iframe has a height of 400 pixels and scrolling enabled.
+    If there is an error fetching the 3D rendering, an error message will be displayed using st.error(). The error
+    message will include the error object that was raised.
+    The function does not return anything, it only displays the 3D rendering or error message.
+
+    Parameters:
+        smile: A string representing the SMILES notation of the molecule to be rendered.
+    """
     molview_url = f"https://embed.molview.org/v1/?mode=balls&smiles={smile}"
     try:
         # Embed 3D rendering of molecule
@@ -16,7 +63,18 @@ def embed_molview(smile: str) -> None:
 
 
 def load_model_in_cache() -> tf.keras.Model:
-    """Load the model into the Streamlit cache."""
+    """This function loads a TensorFlow Keras model into the Streamlit cache, which is a data store that persists
+    across Streamlit app sessions.
+
+    Returns:
+        model: a TensorFlow Keras model object that has been loaded into the Streamlit cache.
+
+    Raises:
+        This function does not raise any exceptions.
+
+    Note:
+        If the model object is already present in the Streamlit cache, this function returns the cached object rather
+        than reloading the model."""
     if "model" not in st.session_state:
         st.session_state["model"] = load_model(filename=None)
     return st.session_state["model"]
@@ -25,7 +83,17 @@ def load_model_in_cache() -> tf.keras.Model:
 def output_for_button(
     button_number: int, samples: list, model: tf.keras.Model
 ) -> float:
-    """Return the prediction for the molecule represented by the given button."""
+    """This function takes in three parameters: button_number, samples, and model. It then uses these
+    parameters to return the prediction for the molecule represented by the given button.
+
+    Parameters:
+        button_number (int): An integer representing the index of the button clicked.
+        samples (list): A list of SMILES strings representing the molecules to predict.
+        model (tf.keras.Model): A trained machine learning model to use for prediction.
+
+    Returns:
+        prediction (float): A float representing the prediction of the machine learning model for the molecule
+        represented by the given button."""
     embed_molview(samples[button_number])
     smiles = samples[button_number]
     smile = []
@@ -36,7 +104,16 @@ def output_for_button(
 
 
 def output_for_string(smiles: str, model: tf.keras.Model) -> float:
-    """Return the prediction for the molecule represented by the given string."""
+    """This function takes a SMILES string representation of a molecule and a trained TensorFlow Keras model, and
+    returns a float prediction for the activity of the molecule.
+
+    Parameters:
+        smiles (str): A string representing the SMILES of a molecule.
+        model (tf.keras.Model): A trained TensorFlow Keras model to use for making predictions.
+
+    Returns:
+        prediction (float): The predicted activity score for the molecule represented by the input SMILES string.
+    """
     embed_molview(smiles)
     smile = []
     smile.append(smiles)
@@ -46,11 +123,22 @@ def output_for_string(smiles: str, model: tf.keras.Model) -> float:
 
 
 def display_desc_instr() -> None:
-    """Display the description and instructions for the website."""
+    """This function displays a description and instructions for the website.
+
+    Arguments: None
+
+    Returns: None
+
+    The function displays a header describing the blood-brain barrier, which is followed by a section of instructions
+    for the website. The instructions include information on what a SMILES string is, the prediction that the website
+    will make based on the input SMILES string, and how to use the sample buttons provided to test the functionality of
+    the website. Finally, the function prompts the user to click on the "Run the Model" button to try out the prediction
+    feature."""
+    description = """The blood - brain barrier is a protective layer that separates the brain from the rest of the 
+    body’s circulatory system. This barrier is highly selective and prevents solutes in the circulating blood from 
+    non-selectively crossing into the extracellular fluid of the central nervous system where neurons reside."""
     st.markdown(
-        (
-            f'<h2 style="color:#FFFFFF;font-size:18px;">{"The blood - brain barrier is a protective layer that separates the brain from the rest of the body’s circulatory system. This barrier is highly selective and prevents solutes in the circulating blood from non-selectively crossing into the extracellular fluid of the central nervous system where neurons reside."}</h2>'
-        ),
+        f'<h2 style="color:#FFFFFF;font-size:18px;">{description}</h2>',
         unsafe_allow_html=True,
     )
     st.markdown(
@@ -58,37 +146,38 @@ def display_desc_instr() -> None:
         unsafe_allow_html=True,
     )
     # with st.expander("Click here to see instructions"):
-    instruction_1 = (
-        "A SMILES string is a representation of the molecule as an ASCII string."
-    )
-    instruction_2 = (
+    inst_1 = "A SMILES string is a representation of the molecule as an ASCII string."
+    inst_2 = (
         "When you give us the SMILE string for a molecule, we will predict its"
         " permeability through the blood-brain barrier and render an interactive"
         " molecular structure in 3d."
     )
-    instruction_3 = (
+    inst_3 = (
         "There are 4 sample buttons below the input box to help demonstrate the"
         " functionality of our website"
     )
-    instruction_4 = "Click on 'Run the Model' to try it out!"
+    inst_4 = "Click on 'Run the Model' to try it out!"
     st.markdown(
         (
-            f'<ul style="color:#FFFFFF"><li>{instruction_1}</li><li>{instruction_2}</li><li>{instruction_3}</li><li>{instruction_4}</li></ul>'
+            f'<ul style="color:#FFFFFF"><li>{inst_1}</li><li>{inst_2}</li><li>{inst_3}</li><li>{inst_4}</li></ul>'
         ),
         unsafe_allow_html=True,
     )
 
 
 def display_goal() -> None:
-    """Display the goal of the project."""
+    """Display the title of the project and its goal on the Streamlit website.
+
+    Parameters: None
+
+    Returns: None"""
+    goal = """The goal of the project is to predict the permeability of molecules through the blood-brain barrier."""
     st.markdown(
         f'<h1 style="color:#FFFFFF;font-size:35px;">{"Graph Brain"}</h1>',
         unsafe_allow_html=True,
     )
     st.markdown(
-        (
-            f'<h2 style="color:#FFFFFF;font-size:20px;">{"""The goal of the project is to predict the permeability of molecules through the blood-brain barrier."""}</h2>'
-        ),
+        (f'<h2 style="color:#FFFFFF;font-size:20px;">{goal}</h2>'),
         unsafe_allow_html=True,
     )
 
@@ -105,3 +194,13 @@ def set_background_black() -> None:
          """,
         unsafe_allow_html=True,
     )
+
+
+# Checking the code for errors using python_ta.
+pyta.check_all(
+    config={
+        "extra-imports": ["streamlit", "PIL", "python_ta"],
+        "allowed-io": [],
+        "max-line-length": 120,
+    }
+)
