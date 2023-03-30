44343bf828208a71f84ca18c82a206c668b87ed0
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-03-30 00:32:11 -0400

    Added docstring, type annotations and python_ta

--- model/configuration/training.py
+++ model/configuration/training.py
@@ -1,12 +1,36 @@
+"""CSC111 Winter 2023 Final Project: Graph Brain
+
+This Python file defines useful function for training the model.
+
+The application has the following functions:
+- atoms() -> dict[str, set]: Return a dictionary of atom features.
+- bonds() -> dict[str, set]: Return a dictionary of bond features.
+- edge_network() -> dict[str, str]: Return a dictionary of edge network features.
+- model() -> dict[str, str | int | list[int] | list[str] | bool | float | None]:
+    Return a dictionary of model features.
+
+Copyright and Usage Information
+===============================
+This file is provided solely for the personal and private use of TAs, instructors and its author(s). All forms of
+distribution of this code, whether as given or with any changes, are expressly prohibited.
+
+This file is Copyright (c) 2023 by Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde."""
+
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from dataset.download_elements import download_periodic
 import csv
+import python_ta as pyta
 
 
-def atoms():
+def atoms() -> dict[str, set]:
+    """Return a dictionary of atom features.
+
+    Returns:
+        A dictionary of atom features.
+    """
     filename = download_periodic()
     symbols = set()
     with open(filename, "r") as file:
@@ -35,21 +59,36 @@ def atoms():
     }
 
 
-def bonds():
+def bonds() -> dict[str, set]:
+    """Return a dictionary of bond features.
+
+    Returns:
+        A dictionary of bond features.
+    """
     return {
         "bond_type": {"single", "double", "triple", "aromatic"},
         "conjugation_state": {True, False},
     }
 
 
-def edge_network():
+def edge_network() -> dict[str, str]:
+    """Return a dictionary of edge network features.
+
+    Returns:
+        A dictionary of edge network features.
+    """
     return {
         "kernel_initializer": "glorot_uniform",
         "bias_initializer": "zeros",
     }
 
 
-def model():
+def model() -> dict[str, str | int | list[int] | list[str] | bool | float | None]:
+    """Return a dictionary of model features.
+
+    Returns:
+        A dictionary of model features.
+    """
     # At the moment we only support the following few naive customizations.
     return {
         # Either GRU, LSTM, SimpleRNN or StackedRNN
@@ -93,3 +132,12 @@ def model():
         # CentralStorageStrategy, ParameterServerStrategy
         "strategy": "TPUStrategy",
     }
+
+pyta.check_all(
+    config={
+        "extra-imports": ["csv", "sys", "os", "python_ta"],
+        "allowed-io": [],
+        "max-line-length": 120,
+    },
+    output="pyta_output12.txt",
+)
