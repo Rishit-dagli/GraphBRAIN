82361eba45689d5d70c127809d70afed3f94e0bd
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-03-30 00:25:58 -0400

    Added docstring, type annotations and python_ta

--- model/dataset/download_dataset.py
+++ model/dataset/download_dataset.py
@@ -1,15 +1,26 @@
 import wget
 import hashlib
 import os
+import python_ta as pyta
 
 
 def _download() -> str:
+    """Download the BBBP dataset from the Rishit's website.
+
+    Returns:
+        The filename of the downloaded dataset.
+    """
     url = "http://store.rishit.tech/BBBP.csv"
     filename = wget.download(url)
     return filename
 
 
 def _check_md5(filename: str) -> bool:
+    """Check the MD5 checksum of the downloaded dataset.
+
+    Returns:
+        True if the MD5 checksum matches, False otherwise.
+    """
     expected_md5 = "66286cb9e6b148bd75d80c870df580fb"
     with open(filename, "rb") as f:
         actual_md5 = hashlib.md5(f.read()).hexdigest()
@@ -17,6 +28,11 @@ def _check_md5(filename: str) -> bool:
 
 
 def download_dataset() -> str:
+    """Download the BBBP dataset if it is not already downloaded.
+
+    Returns:
+        The filename of the downloaded dataset.
+    """
     if not os.path.exists("BBBP.csv"):
         print("Downloading BBBP dataset...")
         filename = _download()
@@ -28,3 +44,13 @@ def download_dataset() -> str:
     else:
         print("BBBP dataset already downloaded")
         return "BBBP.csv"
+
+
+pyta.check_all(
+    config={
+        "extra-imports": ["wget", "hashlib", "os", "python_ta"],
+        "allowed-io": [],
+        "max-line-length": 120,
+    },
+    output="pyta_output11.txt",
+)
