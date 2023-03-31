f1953d5b7c9cff66584c4dbdf752e887e88981f3
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-03-30 23:50:32 -0400

    Fixed license,docstrings and type annotations in model/configuration/training.py

--- model/configuration/training.py
+++ model/configuration/training.py
@@ -1,20 +1,22 @@
 """CSC111 Winter 2023 Final Project: Graph Brain
 
-This Python file defines useful function for training the model.
-
-The application has the following functions:
-- atoms() -> dict[str, set]: Return a dictionary of atom features.
-- bonds() -> dict[str, set]: Return a dictionary of bond features.
-- edge_network() -> dict[str, str]: Return a dictionary of edge network features.
-- model() -> dict[str, str | int | list[int] | list[str] | bool | float | None]:
-    Return a dictionary of model features.
+This Python file fetches the configurations for the model.
 
 Copyright and Usage Information
 ===============================
-This file is provided solely for the personal and private use of TAs, instructors and its author(s). All forms of
-distribution of this code, whether as given or with any changes, are expressly prohibited.
+Copyright 2023 Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde
 
-This file is Copyright (c) 2023 by Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde."""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."""
 
 import sys
 import os
@@ -23,13 +25,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from dataset.download_elements import download_periodic
 import csv
 import python_ta as pyta
+from typing import Union
 
 
 def atoms() -> dict[str, set]:
-    """Return a dictionary of atom features.
+    """Returns a dictionary of atom features.
 
     Returns:
-        A dictionary of atom features.
+        A dictionary containing the following keys and sets of values:
+        - 'element': A set of strings representing the atomic symbols.
+        - 'valence_electrons': A set of integers representing the possible valence electron counts.
+        - 'hydrogen_bonds': A set of integers representing the possible numbers of hydrogen bonds.
+        - 'orbital_hybridization': A set of strings representing the possible orbital hybridizations.
     """
     filename = download_periodic()
     symbols = set()
@@ -60,10 +67,12 @@ def atoms() -> dict[str, set]:
 
 
 def bonds() -> dict[str, set]:
-    """Return a dictionary of bond features.
+    """Returns a dictionary of bond features.
 
     Returns:
-        A dictionary of bond features.
+        A dictionary containing the following keys and sets of values:
+        - 'bond_type': A set of strings representing the possible bond types.
+        - 'conjugation_state': A set of booleans representing the possible conjugation states.
     """
     return {
         "bond_type": {"single", "double", "triple", "aromatic"},
@@ -72,10 +81,12 @@ def bonds() -> dict[str, set]:
 
 
 def edge_network() -> dict[str, str]:
-    """Return a dictionary of edge network features.
+    """Returns a dictionary of edge network features.
 
     Returns:
-        A dictionary of edge network features.
+        A dictionary containing the following keys and string values:
+        - 'kernel_initializer': A string representing the type of kernel initializer.
+        - 'bias_initializer': A string representing the type of bias initializer.
     """
     return {
         "kernel_initializer": "glorot_uniform",
@@ -83,11 +94,37 @@ def edge_network() -> dict[str, str]:
     }
 
 
-def model() -> dict[str, str | int | list[int] | list[str] | bool | float | None]:
-    """Return a dictionary of model features.
+def model() -> dict[str, Union[str, int, list[Union[int, str]], bool, float, None]]:
+    """Returns a dictionary of model features.
 
     Returns:
-        A dictionary of model features.
+        A dictionary containing the following keys and values of various types:
+        - 'edge_update': A string representing the type of edge update.
+        - 'batch_size': An integer representing the batch size.
+        - 'message_units': An integer representing the number of message units.
+        - 'message_steps': An integer representing the number of message steps.
+        - 'num_attention_heads': An integer representing the number of attention heads.
+        - 'dense_units': A list of integers representing the units of dense layers.
+        - 'activation': A list of strings representing the types of activation functions.
+        - 'loss': A string representing the type of loss function.
+        - 'optimizer': A string representing the type of optimizer.
+        - 'learning_rate': A float representing the learning rate.
+        - 'beta_1': A float representing the value of the beta_1 parameter.
+        - 'beta_2': A float representing the value of the beta_2 parameter.
+        - 'epsilon': A float representing the value of the epsilon parameter.
+        - 'weight_decay': Either None or a float representing the weight decay.
+        - 'momentum': Either None or a float representing the momentum.
+        - 'nesterov': A boolean representing whether or not to use Nesterov momentum.
+        - 'clipnorm': Either None or a float representing the maximum norm for gradient clipping.
+        - 'clipvalue': Either None or a float representing the maximum absolute value for gradient clipping.
+        - 'use_ema': A boolean representing whether or not to use exponential moving average.
+        - 'ema_momentum': A float representing the momentum for exponential moving average.
+        - 'metrics': A list of strings representing the types of evaluation metrics.
+        - 'tensorboard': A boolean representing whether or not to use TensorBoard.
+        - 'plot_model': A boolean representing whether or not to plot the model architecture.
+        - 'save_model': A boolean representing whether or not to save the trained model.
+        - 'epochs': An integer representing the number of training epochs.
+        - 'strategy': A string representing the type of strategy for distributed training.
     """
     # At the moment we only support the following few naive customizations.
     return {
@@ -136,7 +173,7 @@ def model() -> dict[str, str | int | list[int] | list[str] | bool | float | None
 
 pyta.check_all(
     config={
-        "extra-imports": ["csv", "sys", "os", "python_ta"],
+        "extra-imports": ["csv", "sys", "os", "typing", "python_ta"],
         "allowed-io": ["atoms"],
         "max-line-length": 120,
     },
