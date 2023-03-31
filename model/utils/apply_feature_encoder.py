a5909952a8e2e661f54eb402e58617c47c851d06
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-03-31 01:04:41 -0400

    Fixed license,docstrings and type annotations in model/utils/apply_feature_encoder.py

--- model/utils/apply_feature_encoder.py
+++ model/utils/apply_feature_encoder.py
@@ -1,19 +1,22 @@
 """CSC111 Winter 2023 Final Project: Graph Brain
 
-This Python file defines useful function for applying feature encoders.
-
-The application has the following functions:
-
-atom_feature_encoder() -> AtomFeatureEncoder: Return an AtomFeatureEncoder object
-.
-bond_feature_encoder() -> BondFeatureEncoder: Return a BondFeatureEncoder object.
+This Python file defines functions for applying feature encoders.
 
 Copyright and Usage Information
 ===============================
-This file is provided solely for the personal and private use of TAs, instructors and its author(s). All forms of
-distribution of this code, whether as given or with any changes, are expressly prohibited.
+Copyright 2023 Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde
 
-This file is Copyright (c) 2023 by Pranjal Agrawal, Rishit Dagli, Shivesh Prakash and Tanmay Shinde."""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."""
 
 from model.utils.feature_encoder import AtomFeatureEncoder, BondFeatureEncoder
 import sys
@@ -25,25 +28,19 @@ from configuration.training import atoms, bonds
 
 
 def atom_feature_encoder() -> AtomFeatureEncoder:
-    """Return an AtomFeatureEncoder object.
-
-    Arguments:
-        None
+    """Returns an AtomFeatureEncoder object.
 
     Returns:
-        an AtomFeatureEncoder object
+        AtomFeatureEncoder: an AtomFeatureEncoder object
     """
     return AtomFeatureEncoder(allowed_feature_sets=atoms())
 
 
 def bond_feature_encoder() -> BondFeatureEncoder:
-    """Return a BondFeatureEncoder object.
-
-    Arguments:
-        None
+    """Returns a BondFeatureEncoder object.
 
     Returns:
-        a BondFeatureEncoder object
+        BondFeatureEncoder: a BondFeatureEncoder object
     """
     return BondFeatureEncoder(allowed_feature_sets=bonds())
 
