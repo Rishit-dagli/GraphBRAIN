1a67f7227775f0b0fe4ac525decc6c91fb168ca7
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2023-04-01 22:16:51 -0400

    Fixing lint errors

--- model/utils/feature_encoder.py
+++ model/utils/feature_encoder.py
@@ -195,6 +195,7 @@ class BondFeatureEncoder(FeatureEncoder):
 
 if __name__ == "__main__":
     import python_ta as pyta
+
     path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     pyta.check_all(
         os.path.join(path, "model", "utils", "feature_encoder.py"),
